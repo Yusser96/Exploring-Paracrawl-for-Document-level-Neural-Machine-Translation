c12a51c3cbb40a94ea31dceb749f42608a51b280
Yusser96 <115434241+Yusser96@users.noreply.github.com>
2023-04-19 05:50:40 +0200

    Add files via upload

--- data_scripts/clean_data.sh
+++ data_scripts/clean_data.sh
@@ -1,18 +1,18 @@
 python splitdocdata.py all.src.parasent.oneline.uniq.train.multiline all.tgt.parasent.oneline.uniq.train.multiline
-# python splitdocdata.py all.src.parasent.oneline.uniq.test.multiline all.tgt.parasent.oneline.uniq.test.multiline
-# python splitdocdata.py all.src.parasent.oneline.uniq.dev.multiline all.tgt.parasent.oneline.uniq.dev.multiline
+python splitdocdata.py all.src.parasent.oneline.uniq.test.multiline all.tgt.parasent.oneline.uniq.test.multiline
+python splitdocdata.py all.src.parasent.oneline.uniq.dev.multiline all.tgt.parasent.oneline.uniq.dev.multiline
 
 counter=1
 while [ $counter -le 50 ]
 do
 echo $counter
 python cleantokenlen.py all.src.parasent.oneline.uniq.train.multiline.$counter all.tgt.parasent.oneline.uniq.train.multiline.$counter &
-# python cleantokenlen.py all.src.parasent.oneline.uniq.test.multiline.$counter all.tgt.parasent.oneline.uniq.test.multiline.$counter &
-# python cleantokenlen.py all.src.parasent.oneline.uniq.dev.multiline.$counter all.tgt.parasent.oneline.uniq.dev.multiline.$counter &
+python cleantokenlen.py all.src.parasent.oneline.uniq.test.multiline.$counter all.tgt.parasent.oneline.uniq.test.multiline.$counter &
+python cleantokenlen.py all.src.parasent.oneline.uniq.dev.multiline.$counter all.tgt.parasent.oneline.uniq.dev.multiline.$counter &
 ((counter++))
 done
-python cleantokenlen.py all.src.parasent.oneline.uniq.test.multiline all.tgt.parasent.oneline.uniq.test.multiline &
-python cleantokenlen.py all.src.parasent.oneline.uniq.dev.multiline all.tgt.parasent.oneline.uniq.dev.multiline &
+# python cleantokenlen.py all.src.parasent.oneline.uniq.test.multiline all.tgt.parasent.oneline.uniq.test.multiline &
+# python cleantokenlen.py all.src.parasent.oneline.uniq.dev.multiline all.tgt.parasent.oneline.uniq.dev.multiline &
 
 wait
 
@@ -21,15 +21,15 @@ while [ $counter -le 50 ]
 do
 echo $counter
 python cleanwronglang.py all.src.parasent.oneline.uniq.train.multiline.$counter.tokclean all.tgt.parasent.oneline.uniq.train.multiline.$counter.tokclean en de &
-# python cleanwronglang.py all.src.parasent.oneline.uniq.test.multiline.$counter.tokclean all.tgt.parasent.oneline.uniq.test.multiline.$counter.tokclean en de &
-# python cleanwronglang.py all.src.parasent.oneline.uniq.dev.multiline.$counter.tokclean all.tgt.parasent.oneline.uniq.dev.multiline.$counter.tokclean en de &
+python cleanwronglang.py all.src.parasent.oneline.uniq.test.multiline.$counter.tokclean all.tgt.parasent.oneline.uniq.test.multiline.$counter.tokclean en de &
+python cleanwronglang.py all.src.parasent.oneline.uniq.dev.multiline.$counter.tokclean all.tgt.parasent.oneline.uniq.dev.multiline.$counter.tokclean en de &
 ((counter++))
 done
-python cleanwronglang.py all.src.parasent.oneline.uniq.test.multiline.tokclean all.tgt.parasent.oneline.uniq.test.multiline.tokclean en de &
-python cleanwronglang.py all.src.parasent.oneline.uniq.dev.multiline.tokclean all.tgt.parasent.oneline.uniq.dev.multiline.tokclean en de &
+# python cleanwronglang.py all.src.parasent.oneline.uniq.test.multiline.tokclean all.tgt.parasent.oneline.uniq.test.multiline.tokclean en de &
+# python cleanwronglang.py all.src.parasent.oneline.uniq.dev.multiline.tokclean all.tgt.parasent.oneline.uniq.dev.multiline.tokclean en de &
 
 wait
 
 python mergesplitdoc.py all.src.parasent.oneline.uniq.train.multiline all.tgt.parasent.oneline.uniq.train.multiline .tokclean.langid
-# python mergesplitdoc.py all.src.parasent.oneline.uniq.test.multiline all.tgt.parasent.oneline.uniq.test.multiline .tokclean.langid
-# python mergesplitdoc.py all.src.parasent.oneline.uniq.dev.multiline all.tgt.parasent.oneline.uniq.dev.multiline .tokclean.langid
+python mergesplitdoc.py all.src.parasent.oneline.uniq.test.multiline all.tgt.parasent.oneline.uniq.test.multiline .tokclean.langid
+python mergesplitdoc.py all.src.parasent.oneline.uniq.dev.multiline all.tgt.parasent.oneline.uniq.dev.multiline .tokclean.langid
